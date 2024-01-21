e4730779cfcdcf0fee1de5fc25847fa18359ff9a
Hyoseung Kim <hyoseung@ucr.edu>
2024-01-20 17:36:21 -0800

    config changed

--- compile.sh
+++ compile.sh
@@ -1,11 +1,5 @@
 #
 # kernel compile for RPi4
 #
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-file=$(which ${CROSS_COMPILE}gcc)
-if [ ! -x "$file" ];
-then 
-	echo "ERROR: Cross compiler not found!"; 
-fi
+source env.sh
 make -j$(nproc) Image modules dtbs 
